ebf2bf4309cf2c031f2e0ea9798bcb4c31d8af8f
elbakali.m <23224639+ElbakaliMourad@users.noreply.github.com>
2026-04-11 17:10:10 -0400

    feat: implement loading state and VIN length validation

--- src/app/components/vehicle-search/vehicle-search.ts
+++ src/app/components/vehicle-search/vehicle-search.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { VehicleService } from '../../services/vehicle';
 import { VehicleSummary } from '../../models/vehicle-summary';
 
@@ -8,48 +7,62 @@ import { VehicleSummary } from '../../models/vehicle-summary';
   standalone: true,
   imports: [],
   templateUrl: './vehicle-search.html',
-  //styleUrl: './vehicle-search.css',
 })
+
 export class VehicleSearch {
 
-  // 1. STATE VARIABLES changed to SIGNALS
   // A signal acts as a wrapper around data that instantly alerts the HTML when it changes.
   vehicleData = signal<VehicleSummary | null>(null);
   errorMessage = signal<string>('');
 
-  // 2. DEPENDENCY INJECTION
+  // Track the loading state
+  isLoading = signal<boolean>(false);
+
+  // DEPENDENCY INJECTION
   // We ask Angular to give us an instance of VehicleService
   constructor(private vehicleService: VehicleService) { }
 
-  // 3. THE SEARCH FUNCTION
+  // THE SEARCH FUNCTION
   // This runs when we click the "Decode VIN" button
   onSearch(vin: string) {
-    // 2. USE .set() TO UPDATE SIGNALS
+    // USE .set() TO UPDATE SIGNALS
     this.errorMessage.set('');
     this.vehicleData.set(null);
 
-    // Safety check: Don't search if the box is empty
-    if (!vin) {
-      this.errorMessage.set('Please enter a VIN.');
+    // Clean the string and check for exactly 17 characters
+    const cleanVin = vin.trim();
+    if (cleanVin.length !== 17) {
+      this.errorMessage.set('A valid VIN must be exactly 17 characters long.');
       return;
     }
 
-    // 4. THE SUBSCRIPTION
+    // Turn on the loading spinner
+    this.isLoading.set(true);
+
+    // // Safety check: Don't search if the box is empty
+    // if (!vin) {
+    //   this.errorMessage.set('Please enter a VIN.');
+    //   return;
+    // }
+
+    // THE SUBSCRIPTION
     // We call service, which returns the Observable (the empty pipe).
     // .subscribe() attaches us to the end of the pipe to wait for the data.
-    this.vehicleService.getVehicleDetails(vin).subscribe({
+    this.vehicleService.getVehicleDetails(cleanVin).subscribe({
 
       // If Java sends back a 200 OK success response...
       next: (data) => {
         console.log("SUCCESS! Data received from Java:", data);
         // This .set() guarantees the HTML will instantly redraw
         this.vehicleData.set(data);
+        this.isLoading.set(false);
       },
 
       // If Java sends back a 404 Not Found or 500 Error...
       error: (err) => {
         console.error("ERROR:", err);
         this.errorMessage.set('Could not find that vehicle. Check the VIN and try again.');
+        this.isLoading.set(false);
       }
 
     });
